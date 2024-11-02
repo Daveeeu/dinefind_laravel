f39ec0279b3ce137ccd153aa5bed5732c9a9015a
david.jandl <david.jandl@smartleap.hu>
2024-11-02 09:17:19 +0100

    lazy loading doc

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -10,16 +10,24 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class RestaurantController extends Controller
 {
-    public function index(Request $request): JsonResource
-    {
-        $limit = $request->query('limit', 10);
-        $offset = $request->query('offset', 0);
+/**
+ * Display a listing of the restaurants.
+ *
+ * Example URL: /api/restaurants?limit=10&offset=0
+ *
+ * @param  Request  $request
+ * @return JsonResource
+ */
+public function index(Request $request): JsonResource
+{
+    $limit = $request->query('limit', 10);
+    $offset = $request->query('offset', 0);
 
-        $restaurants = Restaurant::with('foods.allergens')
-            ->skip($offset)
-            ->take($limit)
-            ->get();
+    $restaurants = Restaurant::with('foods.allergens')
+        ->skip($offset)
+        ->take($limit)
+        ->get();
 
-        return RestaurantResource::collection($restaurants);
-    }
+    return RestaurantResource::collection($restaurants);
+}
 }
